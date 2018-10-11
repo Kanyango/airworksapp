0b9f7b3d64185deb45f629c40f3378770cd1b9b9
Kanyango <kariukikanyango@gmail.com>
2018-10-11 14:29:56 +0300

    Create login.ts

---
+++ src/app/login/login.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit , AfterViewInit, ElementRef} from '@angular/core';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+ 
+
+@Component({
+    selector: 'login-component',
+    templateUrl: './login.html'
+})
+
+export class LoginComponent implements OnInit
+{
+    
+   
+  
+}
