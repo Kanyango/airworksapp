88f6c51699038ced7f14e8ca199fbc27dc2ea811
Kanyango <kariukikanyango@gmail.com>
2018-10-17 20:22:07 +0300

    Update acstatus.ts

--- src/app/reports/acstatus.ts
+++ src/app/reports/acstatus.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import * as jsPDF from 'jspdf';
+import * as html2canvas from 'html2canvas';
 
 @Component({
     selector: 'acstatus-component',
@@ -7,5 +9,16 @@ import { Component, OnInit, Input } from '@angular/core';
 
 export class ACStatusComponent
 {
+  
+  print()
+    {
+        html2canvas(document.getElementById('status')).then(function(canvas)
+        {
+            var img = canvas.toDataURL("image/png");
+            var doc = new jsPDF();
+            doc.addImage(img,'JPEG',0,20);
+            doc.save('testCanvas.pdf');
+        });
+    }
 
 }
