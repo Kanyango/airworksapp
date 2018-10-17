76938253a3e21d9aaa8912259be701f7386de51c
Kanyango <kariukikanyango@gmail.com>
2018-10-17 18:45:46 +0300

    Create acstatus.ts

---
+++ src/app/reports/acstatus.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+    selector: 'acstatus-component',
+    templateUrl: './acstatus.html'
+})
+
+export class ACStatusComponent
+{
+
+}
