03185c96e14a0827539d16a740e4085daec007e9
Kanyango <kariukikanyango@gmail.com>
2018-10-17 14:01:54 +0300

    Create dash.ts

---
+++ src/app/inventory/dash/dash.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+
+@Component({
+    selector: 'dashinv-component',
+    templateUrl: './dashinv.html'
+})
+
+export class DashInventoryComponent
+{
+    id = '';
+    constructor(private route: ActivatedRoute){}
+    
+    ngOnInit()
+     {
+        this.id = this.route.snapshot.paramMap.get('id');
+        console.log('iam',this.id);
+     }
+
+}
