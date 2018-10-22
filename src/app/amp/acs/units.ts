6e063dabfaa15d14437c7044623bfe40000e32c0
Kanyango <kariukikanyango@gmail.com>
2018-10-22 10:59:14 +0300

    Update units.ts

--- src/app/amp/acs/units.ts
+++ src/app/amp/acs/units.ts
@@ -1,1 +1,36 @@
+import {Component} from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import {FormGroup,FormControl,FormBuilder} from '@angular/forms'; 
 
+@Component({
+  selector: 'acunit-component',
+  templateUrl: './unit.html'
+})
+
+export class NewAircraftUnitComponent {
+  closeResult: string;
+  newAcForm:FormGroup; 
+  
+  constructor(private modalService: NgbModal, private fb: FormBuilder) 
+  {}
+
+ 
+  open(content) {
+    this.modalService.open(content, {size: 'lg'})
+    .result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+}
