a285fa591d0915141bf482939f1113b43081d6c3
Kanyango <kariukikanyango@gmail.com>
2018-10-22 13:35:46 +0300

    Update routes.ts

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -1,1 +1,24 @@
 
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+    selector: 'routes-component',
+    templateUrl: './routes.html'
+})
+
+export class RoutesComponent
+{
+    routes = [
+            {
+                ac_reg : '5Y-JWD',
+                model  : 'Bombardier DHC-8-200',
+                serial_no : '447'
+            },
+            {
+                ac_reg : '5Y-ASA',
+                model  : 'Cessna 208 B',
+                serial_no : '337'
+            }
+           ]
+}
