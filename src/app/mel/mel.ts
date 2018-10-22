9d924c90c9e1c5759d3d182306b7fe6aecabadfa
Kanyango <kariukikanyango@gmail.com>
2018-10-22 12:45:05 +0300

    Update mel.ts

--- src/app/mel/mel.ts
+++ src/app/mel/mel.ts
@@ -1,1 +1,24 @@
 
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+    selector: 'mels-component',
+    templateUrl: './mel.html'
+})
+
+export class MELsComponent
+{
+    mel = [
+            {
+                ac_reg : '5Y-JWD',
+                model  : 'Bombardier DHC-8-200',
+                serial_no : '447'
+            },
+            {
+                ac_reg : '5Y-ASA',
+                model  : 'Cessna 208 B',
+                serial_no : '337'
+            }
+           ]
+}
