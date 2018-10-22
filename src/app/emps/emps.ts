86cc85d69cc8a84a0ecd60baba9e436bf655615a
Kanyango <kariukikanyango@gmail.com>
2018-10-22 14:21:42 +0300

    Update emps.ts

--- src/app/emps/emps.ts
+++ src/app/emps/emps.ts
@@ -1,1 +1,24 @@
 
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+    selector: 'emps-component',
+    templateUrl: './emps.html'
+})
+
+export class EmpsComponent
+{
+    emps = [
+            {
+                ac_reg : '5Y-JWD',
+                model  : 'Bombardier DHC-8-200',
+                serial_no : '447'
+            },
+            {
+                ac_reg : '5Y-ASA',
+                model  : 'Cessna 208 B',
+                serial_no : '337'
+            }
+           ]
+}
