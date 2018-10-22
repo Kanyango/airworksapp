5eed0fade31b7ca525091e840ec5c1b26d4c1e72
Kanyango <kariukikanyango@gmail.com>
2018-10-22 13:38:21 +0300

    Update newroute.ts

--- src/app/routes/newroute.ts
+++ src/app/routes/newroute.ts
@@ -1,1 +1,37 @@
 
+import {Component} from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import {FormGroup,FormControl,FormBuilder} from '@angular/forms'; 
+
+@Component({
+  selector: 'newroute-component',
+  templateUrl: './newroute.html'
+})
+
+export class NewRouteComponent {
+  closeResult: string;
+  newAcForm:FormGroup; 
+  
+  constructor(private modalService: NgbModal, private fb: FormBuilder) 
+  {}
+
+  
+  open(content) {
+    this.modalService.open(content, {size: 'lg'})
+    .result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+}
