2c276e60221d62f7442faaab9696c786789bbf3e
Kanyango <kariukikanyango@gmail.com>
2018-10-23 15:24:25 +0300

    Update workds.ts

--- src/app/amp/wokds/workds.ts
+++ src/app/amp/wokds/workds.ts
@@ -8,5 +8,34 @@ import { ActivatedRoute } from '@angular/router';
 
 export class WorkOrdersComponent
 {
+  
+  
+    tasks = [
+                {
+                    task_no: '2750/02',
+                    desc   : 'FLAP PRIMARY TORQUE SHAFT SPLINES',
+                    action : 'LUBE',
+                    every  : '10000',
+                    period : 'CYCLES',
+                    last   : '38505',
+                    next   : '48505',
+                    togo   : '9314',
+                    ac     : '5Y-JWD',
+                    sn     : '447'
+                },
+                {
+                    task_no: '2750/18',
+                    desc   : 'FLAP BALLSCREW ACTUACTORS BACKLASH CHK',
+                    action : 'FUNC',
+                    every  : '3000',
+                    period : 'CYCLES',
+                    last   : '38505',
+                    next   : '48505',
+                    togo   : '9314',
+                    ac     : '5Y-JWD',
+                    sn     : '447'
+                }
+            ]
+
 
 }
