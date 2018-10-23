36ac0d3603b5a2176de2fb7b10a7c9ad6814ea9a
Kanyango <kariukikanyango@gmail.com>
2018-10-23 11:37:01 +0300

    Update newkod.ts

--- src/app/amp/wokds/newkod.ts
+++ src/app/amp/wokds/newkod.ts
@@ -1,1 +1,18 @@
 
+import {Component} from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import {FormGroup,FormControl,FormBuilder} from '@angular/forms'; 
+
+@Component({
+  selector: 'newkod-component',
+  templateUrl: './newkod.html'
+})
+
+export class NewWKDComponent {
+  closeResult: string;
+  taskCardForm:FormGroup; 
+  
+  constructor(private modalService: NgbModal, private fb: FormBuilder) {}
+
+  
+}
