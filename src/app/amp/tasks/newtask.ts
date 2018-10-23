0ff9b2410a32dcc9b6db685164685394f71566c6
Kanyango <kariukikanyango@gmail.com>
2018-10-23 11:04:21 +0300

    Update newtask.ts

--- src/app/amp/tasks/newtask.ts
+++ src/app/amp/tasks/newtask.ts
@@ -11,39 +11,7 @@ export class NewTaskComponent {
   closeResult: string;
   taskCardForm:FormGroup; 
   
-  constructor(private modalService: NgbModal, private fb: FormBuilder) 
-  {
-    this.createForm()
-  }
+  constructor(private modalService: NgbModal, private fb: FormBuilder) {}
 
-  createForm()
-  {
-    this.taskCardForm = this.fb.group(
-    {
-        task_no: [''],
-        description: [''],
-        part_no  : [''],
-        serial_no: [''],
-        action: [''],
-        period: ['']
-    })
-  }
-  open(content) {
-    this.modalService.open(content, {size: 'lg'})
-    .result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return  `with: ${reason}`;
-    }
-  }
+  
 }
