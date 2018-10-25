fe9516b29a2c574fdbf1290697c23db39c9707f6
Kanyango <kariukikanyango@gmail.com>
2018-10-25 13:05:57 +0300

    Update newpart.ts

---
+++ src/app/inventory/newpart.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+    selector: 'newpart-component',
+    templateUrl: './newpart.html'
+})
+
+export class NewInvPartComponent 
+{
+    
+}
