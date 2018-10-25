de86748ed08ef3178b676463b346a4bed7fd0b47
Kanyango <kariukikanyango@gmail.com>
2018-10-25 19:37:25 +0300

    Create receive.ts

---
+++ src/app/receive/receive.ts
@@ -0,0 +1,59 @@
+<navbar-component></navbar-component>
+<br>
+<br>
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="card">
+        <div class="card-body">
+          <h3 class="card-title">Receiving P/O</h3>
+          <br>
+          <ngb-tabset>
+              <ngb-tab title="General">
+                <ng-template ngbTabContent>
+                
+                </ng-template>
+              </ngb-tab>
+              <ngb-tab>
+                <ng-template ngbTabTitle>Warranty</ng-template>
+                <ng-template ngbTabContent>
+                
+                </ng-template>
+              </ngb-tab>
+              <ngb-tab title="Shipping">
+                <ng-template ngbTabContent>
+                
+                </ng-template>
+              </ngb-tab>
+              <ngb-tab title="Certificate">
+                <ng-template ngbTabContent>
+                
+                </ng-template>
+              </ngb-tab>
+              <ngb-tab title="Tag">
+                <ng-template ngbTabContent>
+                
+                </ng-template>
+              </ngb-tab>
+              <ngb-tab title="Times">
+                <ng-template ngbTabContent>
+                
+                </ng-template>
+              </ngb-tab>
+              <ngb-tab title="P/N Change">
+                <ng-template ngbTabContent>
+                
+                </ng-template>
+              </ngb-tab>
+              <ngb-tab title="Discrepancy">
+                <ng-template ngbTabContent>
+                
+                </ng-template>
+              </ngb-tab>
+          </ngb-tabset>
+          <div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
