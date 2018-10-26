953faf00dc98edccaec2989c4618116adfba7c85
Kanyango <kariukikanyango@gmail.com>
2018-10-26 12:34:43 +0300

    Create newreq.ts

---
+++ src/app/req/newreq.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+    selector: 'newreq-component',
+    templateUrl: './newreq.html'
+})
+
+export class RequisitionComponent
+{}
