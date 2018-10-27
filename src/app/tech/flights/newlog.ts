62073e4f55fbea6f2c94f62e865ea86080aff999
Kanyango <kariukikanyango@gmail.com>
2018-10-27 09:30:52 +0300

    Update newlog.ts

--- src/app/tech/flights/newlog.ts
+++ src/app/tech/flights/newlog.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'newlog-component',
@@ -8,6 +9,37 @@ import { ActivatedRoute } from '@angular/router';
 
 export class NewACLogsComponent
 {
-
-
+  
+  flightLogForm: FormGroup;
+  
+  constructor(private fb: FormBuilder)
+  {
+    this.createForm()
+  }
+  
+  
+  createForm()
+  {
+    this.flightLogForm = this.fb.group(
+      {
+        ac : ['',Validators.required],
+        log : ['',Validators.required],
+        leg : ['',Validators.required],
+        date : ['',Validators.required],
+        off : ['',Validators.required],
+        to : ['',Validators.required],
+        lng : ['',Validators.required],
+        on : ['',Validators.required],
+        cycles : ['',Validators.required],
+        pilot : ['',Validators.required],
+        origin : ['',Validators.required],
+        destination : ['',Validators.required],
+        flight : ['',Validators.required],
+        fuel_burned : ['',Validators.required],
+        block : ['',Validators.required],
+        cat_rating : ['',Validators.required],
+        etops : ['',Validators.required],
+        flight_hrs : ['',Validators.required]
+      })
+  }
 }
