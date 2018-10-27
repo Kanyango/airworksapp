2ebfaba49a7919eeb4ed40e5c35c1ecc23bd1d7e
Kanyango <kariukikanyango@gmail.com>
2018-10-27 09:19:43 +0300

    Create models.ts

---
+++ src/app/services/models.ts
@@ -0,0 +1,24 @@
+export class ACFlight
+{
+
+  ac = '';
+  flight_log  = '';
+  leg = '';
+  flight_date = '';
+  off = '';
+  to = '';
+  lng = '';
+  on = '';
+  cycles = '';
+  pilot = '';
+  origin = '';
+  destination = '';
+  flight = '';
+  fuel_burned = '';
+  block = '';
+  cat_rating = '';
+  etops = '';
+  flight_hrs = '';
+  
+  
+}
