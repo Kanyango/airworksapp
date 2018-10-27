0675cf7161caf2857b10778c7b831da2bce996f9
Kanyango <kariukikanyango@gmail.com>
2018-10-27 17:16:34 +0300

    Update newac.ts

--- src/app/amp/acs/newac.ts
+++ src/app/amp/acs/newac.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
-import {FormGroup,FormControl,FormBuilder} from '@angular/forms'; 
+import {FormGroup,FormControl,FormBuilder, Validators} from '@angular/forms'; 
 
 @Component({
   selector: 'newac-component',
@@ -20,28 +20,68 @@ export class NewAircraftComponent {
   {
     this.newAcForm = this.fb.group(
     {
-        ac_reg: [''],
-        serial_no: [''],
-        model: [''],
-        build_date: ['']
+        status : ['', Validators.required],
+        ac : ['', Validators.required],
+        sn : ['', Validators.required],
+        type : ['', Validators.required],
+        series : ['', Validators.required],
+        build_date : ['', Validators.required],
+        no_of_engines : ['', Validators.required],
+        eol_date : ['', Validators.required],
+        eol_hrs : ['', Validators.required],
+        eol_cycles : ['', Validators.required],
+        auth_no : ['', Validators.required],
+        amm : ['', Validators.required],
+        amp : ['', Validators.required],
+        revision : ['', Validators.required],
+        date : ['', Validators.required],
+      ` curr_ac_flight_hrs : ['', Validators.required],
+        block_hrs : ['', Validators.required],
+        ac_cycles : ['', Validators.required],
+        rin : ['', Validators.required],
+        time_as_of : ['', Validators.required],
+        ac_under_warranty : ['', Validators.required],
+        tped_comp : ['', Validators.required],
+        etops : ['', Validators.required],
+        rsvm : ['', Validators.required],
+        vendor : ['', Validators.required],
+        war_hrs : ['', Validators.required],
+        warr_cycles : ['', Validators.required],
+        warr_days : ['', Validators.required],
+        mnfctr : ['', Validators.required],
+        owner : ['', Validators.required],
+        operator : ['', Validators.required],
+        insurance : ['', Validators.required],
+        last_ac_reg : ['', Validators.required],
+        expiry_date : ['', Validators.required],
+        reg_date : ['', Validators.required],
+        own_cert : ['', Validators.required],
+        flight_status : ['', Validators.required],
+        max_taxi_wei : ['', Validators.required],
+        fuel_cap : ['', Validators.required],
+        boawi_val : ['', Validators.required],
+        freigh_pax_cargo : ['', Validators.required],
+        winglet : ['', Validators.required],
+        max_land_weight : ['', Validators.required],
+        max_to_weight : ['', Validators.required],
+        mac_val : ['', Validators.required],
+        max_thrust : ['', Validators.required],
+        enhancd_cabin : ['', Validators.required],
+        max_zero_fuel_weight : ['', Validators.required],
+        basic_empty_weight : ['', Validators.required],
+        arm_value : ['', Validators.required],
+        amo : ['', Validators.required],
+        ops_empty_weight : ['', Validators.required],
+        total_moment : ['', Validators.required],
+        oew_value : ['', Validators.required],
+        max_cruis_alt : ['', Validators.required],
+        status : ['', Validators.required],
+        engine_model: ['',Validators.required],
+        delivery_date : ['', Validators.required],
+        coa_date : ['', Validators.required],
+        date_on_ops : ['', Validators.required],
+        first_revenue_date : ['', Validators.required]
     })
   }
-  open(content) {
-    this.modalService.open(content, {size: 'lg'})
-    .result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return  `with: ${reason}`;
-    }
-  }
+  
 }
