fbd3a2e9a32ac18e60b2ffeb462a4ef1cdd59623
Kanyango <kariukikanyango@gmail.com>
2018-10-27 18:30:24 +0300

    Update newschedule.ts

--- src/app/tech/schedule/newschedule.ts
+++ src/app/tech/schedule/newschedule.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'newschedule-component',
@@ -7,4 +8,38 @@ import { ActivatedRoute } from '@angular/router';
 })
 
 export class NewSchedulesComponent
-{}
+{
+    newSchedForm: FormGroup;
+  
+  constructor(private fb: FormBuilder)
+  {
+    this.createForm()
+  }
+  
+  createForm()
+  {
+      this.newSchedForm = this.fb.group({
+        
+          status : ['',Validators.required],
+          flight : ['',Validators.required],
+          leg : ['',Validators.required],
+          origin : ['',Validators.required],
+          destination : ['',Validators.required],
+          ac : ['',Validators.required],
+          ac_type : ['',Validators.required],
+          ac_series : ['',Validators.required],
+          pilot : ['',Validators.required],
+          from_schdl_depart : ['',Validators.required],
+          to_schdl_depart : ['',Validators.required],
+          from_schdl_arrival : ['',Validators.required],
+          to_schdl_arrival : ['',Validators.required],
+        
+      })
+  }
+  
+  onSubmit()
+  {
+    console.log(this.newSchedForm.value);
+  }
+
+}
