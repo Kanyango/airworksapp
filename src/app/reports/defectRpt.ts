d4002446a25e30e1d45638288ab229f20487bc0d
Kanyango <kariukikanyango@gmail.com>
2018-10-27 19:09:41 +0300

    Update defectRpt.ts

--- src/app/reports/defectRpt.ts
+++ src/app/reports/defectRpt.ts
@@ -1,1 +1,46 @@
 
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+    selector: 'newdefectrpt-component',
+    templateUrl: './defectRpt.html'
+})
+
+export class NewDefectRPTComponent
+{
+    defectRptForm: FormGroup;
+  
+  constructor(private fb: FormBuilder)
+  {
+    this.createForm()
+  }
+  
+  createForm()
+  {
+      this.newSchedForm = this.fb.group({
+        
+          status : ['',Validators.required],
+          flight : ['',Validators.required],
+          leg : ['',Validators.required],
+          origin : ['',Validators.required],
+          destination : ['',Validators.required],
+          ac : ['',Validators.required],
+          ac_type : ['',Validators.required],
+          ac_series : ['',Validators.required],
+          pilot : ['',Validators.required],
+          from_schdl_depart : ['',Validators.required],
+          to_schdl_depart : ['',Validators.required],
+          from_schdl_arrival : ['',Validators.required],
+          to_schdl_arrival : ['',Validators.required],
+        
+      })
+  }
+  
+  onSubmit()
+  {
+    console.log(this.defectRptForm.value);
+  }
+
+}
