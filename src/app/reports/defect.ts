2f0d42c0060549319d3b1ef2b6daf10f1a7f696a
Kanyango <kariukikanyango@gmail.com>
2018-10-28 13:24:00 +0300

    Update defect.ts

--- src/app/reports/defect.ts
+++ src/app/reports/defect.ts
@@ -7,5 +7,19 @@ import { Component, OnInit, Input } from '@angular/core';
 
 export class DefectComponent
 {
-
+   flights = [{ac: '0810', 
+                flight_log: '2L', 
+                leg: '1', 
+                flight_date: '12/05/2018',
+                of: '10:00',
+                to: '10:00',
+                lng: '13:00',
+                on: '13:00',
+                cycles: '1',
+                origin: 'ACK',
+                destination: 'MIA',
+                flight: '3',
+                block: '3',
+                cat_rating:'CAT'
+                }]
 }
