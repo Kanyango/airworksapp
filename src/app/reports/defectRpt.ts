734a29ca3f53ea69a41bc42a533506fac49708fd
Kanyango <kariukikanyango@gmail.com>
2018-10-28 12:14:25 +0300

    Update defectRpt.ts

--- src/app/reports/defectRpt.ts
+++ src/app/reports/defectRpt.ts
@@ -19,21 +19,37 @@ export class NewDefectRPTComponent
   
   createForm()
   {
-      this.newSchedForm = this.fb.group({
+      this.defectRptForm = this.fb.group({
         
-          status : ['',Validators.required],
-          flight : ['',Validators.required],
-          leg : ['',Validators.required],
-          origin : ['',Validators.required],
-          destination : ['',Validators.required],
+          defect_type : ['',Validators.required],
+          defect : ['',Validators.required],
+          item : ['',Validators.required],
           ac : ['',Validators.required],
-          ac_type : ['',Validators.required],
-          ac_series : ['',Validators.required],
-          pilot : ['',Validators.required],
-          from_schdl_depart : ['',Validators.required],
-          to_schdl_depart : ['',Validators.required],
-          from_schdl_arrival : ['',Validators.required],
-          to_schdl_arrival : ['',Validators.required],
+          chap : ['',Validators.required],
+          sec : ['',Validators.required],
+          par : ['',Validators.required],
+          asc : ['',Validators.required],
+          flight : ['',Validators.required],
+          gate : ['',Validators.required],
+          position : ['',Validators.required],
+          status : ['',Validators.required],
+          defect_cat : ['',Validators.required],
+          rqstd_by : ['',Validators.required],
+          rptd_date : ['',Validators.required],
+          station : ['',Validators.required],
+          auth : ['',Validators.required],
+          defect_desc : ['',Validators.required],
+          est_tat : ['',Validators.required],
+          grnd_time_rqrd : ['',Validators.required],
+          task_card : ['',Validators.required],
+          defer : ['',Validators.required],
+          defer_cat : ['',Validators.required],
+          defer_by : ['',Validators.required],
+          defer_date : ['',Validators.required],
+          mel : ['',Validators.required],
+          cdl : ['',Validators.required],
+          gmm : ['',Validators.required],
+          defer_wo : ['',Validators.required],
         
       })
   }
