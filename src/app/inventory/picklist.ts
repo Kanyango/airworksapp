9d7cb1403afb87ebd2808a0487928b28f3d205c7
Kanyango <kariukikanyango@gmail.com>
2018-10-28 14:32:21 +0300

    Update picklist.ts

--- src/app/inventory/picklist.ts
+++ src/app/inventory/picklist.ts
@@ -1,1 +1,11 @@
 
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+    selector: 'picklist-component',
+    templateUrl: './picklist.html'
+})
+
+export class PickListComponent 
+{}
