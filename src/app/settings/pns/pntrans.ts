849ce045e0f34cc04b777f9269dc6b2de3e87280
Kanyango <kariukikanyango@gmail.com>
2018-10-28 16:05:57 +0300

    Update pntrans.ts

--- src/app/settings/pns/pntrans.ts
+++ src/app/settings/pns/pntrans.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormGroup,FormControl,FormBuilder, Validators, FormArray } from '@angular/forms'; 
 
 @Component({
     selector: 'pntrans-component',
@@ -7,4 +8,34 @@ import { ActivatedRoute } from '@angular/router';
 })
 
 export class PNTransComponent
-{}
+{
+   pnTransForm: FormGroup;
+  
+  constructor(private fb: FormBuilder)
+  {
+      this.createForm()
+  }
+  
+  createForm()
+  {
+     this.pnTransForm = this.fb.group(
+      {  
+        wo : [''],
+        inv_loc: ['', Validators.required],
+        task_card: ['', Validators.required],
+        req_by: ['', Validators.required],
+        priority: ['', Validators.required],
+        deli_loc: [''],
+        deli_site: [''],
+        pns  : this.fb.array([])
+        
+      })
+  
+    }
+  
+  onSubmit()
+  {
+    console.log(this.pnTransForm.value);
+  }
+  
+}
