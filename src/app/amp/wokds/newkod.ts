5d6047da20d6279b50201a1a74779270daf68d05
Kanyango <kariukikanyango@gmail.com>
2018-10-29 15:23:10 +0300

    Update newkod.ts

--- src/app/amp/wokds/newkod.ts
+++ src/app/amp/wokds/newkod.ts
@@ -1,7 +1,7 @@
 
 import {Component} from '@angular/core';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
-import {FormGroup,FormControl,FormBuilder} from '@angular/forms'; 
+import { FormGroup,FormControl,FormBuilder, Validators, FormArray } from '@angular/forms'; 
 
 @Component({
   selector: 'newkod-component',
@@ -10,9 +10,42 @@ import {FormGroup,FormControl,FormBuilder} from '@angular/forms';
 
 export class NewWKDComponent {
   closeResult: string;
-  taskCardForm:FormGroup; 
   
-  constructor(private modalService: NgbModal, private fb: FormBuilder) {}
+  newWokdForm:FormGroup; 
+  
+  constructor(private modalService: NgbModal, private fb: FormBuilder) 
+  {
+    this.createForm()
+  }
+  
+  createForm()
+  {
+     this.newWokdForm = this.fb.group(
+      {  
+ 
+        status: ['', Validators.required],
+        category: ['', Validators.required],
+        location: ['', Validators.required],
+        site: ['', Validators.required],
+        priority: ['', Validators.required],
+        description: ['', Validators.required],
+        ac: ['', Validators.required],
+        sn: ['', Validators.required],
+        tat: ['', Validators.required],
+        tat_date: ['', Validators.required],
+        ssd: ['', Validators.required],
+        sse: ['', Validators.required],
+        ass: ['', Validators.required],
+        ase: ['', Validators.required],
+        description: ['', Validators.required],
+        compltd_by: ['', Validators.required],
+        closd_by: ['', Validators.required],
+        auth_by: ['', Validators.required],
+        auth_date: ['', Validators.required],
+        
+      })
+  
+    }
 
   
 }
