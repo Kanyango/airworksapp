277904b2d3b375cea6f3e1b9c8ede71be733b428
Kanyango <kariukikanyango@gmail.com>
2018-10-29 20:31:12 +0300

    Update taskmont.ts

--- src/app/amp/wokds/taskmont.ts
+++ src/app/amp/wokds/taskmont.ts
@@ -1,19 +1,50 @@
 
 import {Component} from '@angular/core';
-
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { FormGroup,FormControl,FormBuilder, Validators, FormArray } from '@angular/forms'; 
 
 @Component({
   selector: 'taskmont-component',
   templateUrl: './taskmont.html'
 })
 export class WOTaskMontComponent {
+  
   closeResult: string;
+  woTaskForm:FormGroup; 
   
   tasks = [{name: "Task 1"}, {name: "Task 2"}]
 
-  constructor(private modalService: NgbModal) {}
-
+  constructor(private modalService: NgbModal)
+  {
+    this.creatForm()
+  }
+  
+  creatForm()
+  {
+    this.woTaskForm = this.fb.group(
+      {  
+ 
+        ac: ['', Validators.required],
+        wo_no: ['', Validators.required],
+        tc_no: ['', Validators.required],
+        pn: ['', Validators.required],
+        pn_sn: ['', Validators.required],
+        rev: ['', Validators.required],
+        cat: ['', Validators.required],
+        type: ['', Validators.required],
+        desc: ['', Validators.required],
+        status: ['', Validators.required],
+        priority: ['', Validators.required],
+        inspctd: ['', Validators.required],
+        RII: ['', Validators.required],
+        desc_work: ['', Validators.required],
+        schd_start_date: ['', Validators.required],
+        schd_end_date: ['', Validators.required],
+        hist_desc: ['', Validators.required]
+        
+      })
+  }
+  
   open(content) {
     this.modalService.open(content, {size: 'lg'}).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
