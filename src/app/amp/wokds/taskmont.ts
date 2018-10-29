043cc932500a929d5852d4df5e7c828ad5051ecc
Kanyango <kariukikanyango@gmail.com>
2018-10-29 16:19:30 +0300

    Update taskmont.ts

--- src/app/amp/wokds/taskmont.ts
+++ src/app/amp/wokds/taskmont.ts
@@ -1,1 +1,34 @@
 
+import {Component} from '@angular/core';
+
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'taskmont-component',
+  templateUrl: './taskmont.html'
+})
+export class WOTaskMontComponent {
+  closeResult: string;
+  
+  tasks = [{name: "Task 1"}, {name: "Task 2"}]
+
+  constructor(private modalService: NgbModal) {}
+
+  open(content) {
+    this.modalService.open(content, {size: 'lg'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+}
